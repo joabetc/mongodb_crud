764fef335ce18d737992c7422add79c96a54c05a
Joabe Costa <joabetc@hotmail.com>
2019-02-13 23:42:55 -0200

    Creating a connection

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const path = require('path');
 
 const db = require('./db');
 const collection = 'todo';
+
+db.connect((err) => {
+  if (err) {
+    console.log('Unable to connect to database');
+    process.exit(1);
+  } else {
+    app.listen(3000, () => {
+      console.log('Connected ti database, app listening on port 3000');
+    });
+  }
+});
