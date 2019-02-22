b9ac3bd08cf88645036e9493a41ef133819cec20
Joabe Costa <joabetc@hotmail.com>
2019-02-22 18:30:51 -0300

    Creating a schema using Joi

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
 const path = require('path');
+const Joi = require('joi');
 
 const db = require('./db');
 const collection = 'todo';
+
+app.use(bodyParser.json());
+const app = express();
+
+const schema = Joi.object().keys({
+  todo: Joi.string().require()
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
